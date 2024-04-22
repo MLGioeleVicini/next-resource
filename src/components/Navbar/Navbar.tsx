5726c53866535327a29d33fadfe25c189d9b404c
Gioele Vicini <gioy00gv@gmail.com>
2024-04-22 19:02:31 +0200

    dev

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,18 @@
-import Link from "next/link";
-import LogoPic from "../Logo/LogoPic";
+import Link from 'next/link';
+import LogoPic from '@/components/Logo/LogoPic';
 
 const Navbar = () => {
     return (
-        <div className="flex justify-between p-8 bg-slate-600 ">
-            <LogoPic width="250" />
-              <Link href="/dashboard">Homepage</Link>
-              <Link href="/list">Lista</Link>
-              <Link href="/edit">Modifica</Link>
-              <Link href="/change-server">Cambia server</Link>
-        </div>
+        <>
+            <div className='flex justify-between p-8 bg-slate-600 '>
+                <LogoPic width='250' />
+                <Link href='/dashboard'>Homepage</Link>
+                <Link href='/list'>Lista</Link>
+                <Link href='/second-list'>Aggiornata</Link>
+                <Link href='/edit'>Modifica</Link>
+                <Link href='/change-server'>Cambia server</Link>
+            </div>
+        </>
     );
 };
 
