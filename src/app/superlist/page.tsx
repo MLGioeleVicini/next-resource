37bb29d1190b3d01b5f2526614a28c26cb60545a
Gioele Vicini <g.vicini@mlinformaticasrl.it>
2024-04-22 08:18:31 +0200

    dev

---
+++ src/app/superlist/page.tsx
@@ -0,0 +1,10 @@
+import TaskTable from "@/components/List/TaskTable"
+
+const SuperList = () => {
+  return (
+      <div>SuperList</div>
+      <TaskTable />
+  )
+} 
+
+export default SuperList
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,6 +8,7 @@ const Navbar = () => {
               <Link href="/dashboard">Homepage</Link>
               <Link href="/list">Lista</Link>
               <Link href="/edit">Modifica</Link>
+              <Link href="/superlist">Modifica</Link>
               <Link href="/change-server">Cambia server</Link>
         </div>
     );
